57eb163d882a8586a2a70188b2d3e723818b0d61
Paul Al-Dabagh <paul.al-dabagh@hva.nl>
2022-10-03 17:46:10 +0200

    init

---
+++ ci/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Demo github actions CI-CD/i);
+  expect(linkElement).toBeInTheDocument();
+});
